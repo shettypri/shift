873e59dc960e0c86e82aab45cae71c7e74e2cc65
AJ <joysondsouzaaj@gmail.com>
2023-06-27 22:50:55 +0530

    Service.js completed

---
+++ src/assets/Data/Service/Service.js
@@ -0,0 +1,42 @@
+import Car_Rental from "../../Images/Service/01_Car_Rental.png"
+import Car_Sharing from "../../Images/Service/02_Car_Sharing.png"
+import e_Chauffeur from "../../Images/Service/03_e_Chauffeur.png"
+import Group_Transportation from "../../Images/Service/04_Group_Transportation.png"
+import Operating_Lease from "../../Images/Service/05_Operating_Lease.png"
+import Last_Mile from "../../Images/Service/06_Last_Mile.png"
+import Linehaul_and_CrossBorder from "../../Images/Service/07_Linehaul_and_CrossBorder.png"
+import Car_Trading from "../../Images/Service/08_Car_Trading.png"
+
+export const Transportation = {
+    title: "Transportation",
+    Images: [
+        Car_Rental, Car_Sharing, e_Chauffeur, Group_Transportation
+    ],
+    Description: [
+        "Classical car rental outlets open in key cities across the Kingdom paired with a fully integrated fleet management system & a customer service center to ensure you are always taken care of" +
+        "Fully automated  rental solution enabling our customers to book,pay, pick up , drop off and rate all at the touch of a button" +
+        "With technology at our core,we give you peace of mind providing you with your own personal chauffeur at a click of a button. We deliver professionalism in service by owning our own fleet & employing our own highly trained chauffeurs. Getting you & your loved ones around safely wherever you need to be" +
+        "Digitally transforming the industry’s buss services; a complete revamp on the customer journey where you rest at ease with your transportation needs, be it in Hajj and Umrah, schooling, or corporate requirements"
+    ],
+}
+
+export const Leasing = {
+    Title: "Leasing",
+    Images: [Operating_Lease],
+    Description: "Tailormade leasing solutions using worldclass technology so you can steer the cost of operations and drive efficiency throughout the organization"
+}
+export const Logistics = {
+    Title: "Logistics",
+    Images: [
+        Last_Mile, Linehaul_and_CrossBorder
+    ],
+    Description: "Capitalizing on the international need for last mile delivery service and the local growth of ecommerce, we introduce our delivery service crowned with world class technology commanding the smoothest operations  A multi modal approach in domestic & international transport to meet your business needs. An efficient service powered by technology providing you with the tools and information required to optimize your business and focus on what matters most"
+}
+
+export const Trading = {
+    Title: "Trading",
+    Images: [
+        Car_Trading
+    ],
+    Description: "Given our breadth of reach in the automotive & transportation business we utilize our resources to fulfill our customers’ vehicle requirements. Ensuring the best quality & price are consistently delivered across our clients’ network throughout the MENRegi"
+}
