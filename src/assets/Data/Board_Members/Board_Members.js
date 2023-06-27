35d2009143d7d8065988964db8a370f1aa3c47cd
AJ <joysondsouzaaj@gmail.com>
2023-06-27 20:56:07 +0530

    Board_Members.js is completed..............................

---
+++ src/assets/Data/Board_Members/Board_Members.js
@@ -0,0 +1,57 @@
+export const Board_Members = [
+    {
+        name: "Abdulkareem Abualnasr",
+        about: "Abdulkareem is an esteemed name in the world of Finance  and the Chairman of Shift inc. A passionate believer in the  value of sustainability and good corporate citizenship. He  sits on numerous boards and has been recognized for his  considerable contribution to the Financial Services in the  Kingdom and the Region.",
+        Work_Experience: [
+            "Previous CEO of NCB for 7 years ",
+            "Member of the Board of ’Abdul Latif Jamil Ltd.  Company’ ",
+            "Chairman of the Audit Committee of ‘Abdul Latif Jameel Ltd. Company’"
+        ],
+        Qualifications: [
+            "BBA - California State University",
+            "MBA - The American University of Cairo The “Lifetime Achievement Award” from the Banker  Magazine in 2008",
+            "Voted the “Islamic Banker of the Year 2010” by The  World Islamic Banking Conference"
+        ]
+    },
+    {
+        name: "Rayan Qutub",
+        about: "Rayan is currently the chairman of Nama Al Baraka  Investments and a board member of several companies,  Rayan is equipped with more than 24 years of Industrial,  Real-Estate, Acquisition, Education, Healthcare,  Marketing & FMCG experience.",
+        Work_Experience: [
+            "CEO of the Ports Development Company at King  Abdullah Economic City (KAEC)",
+            "Member of MBO",
+            "Head of Mergers and Acquisitions in the Middle East, UNILEVER International"
+        ],
+        Qualifications: ["BSC in Business Administration - King Fahd  University of Petroleum & Minerals "]
+    },
+    {
+        name: "Ahmed Y. Jamjoom",
+        about: "Ahmed is a board member at Shift inc. A pharmacist that  started his education in the University of Reading, UK. He  later went on to study at Ibn Sina Medical College KSA,  where he completed his MPharm degree and has been  involved with the Jamjoom Group business since 2008.  Ahmed also has experience in the fields of supply chain,  logistics and procurement in the pharmaceutical industry",
+        Work_Experience: [
+            "Member of the board of “Abdullatif M.S Jamjoom &  Brothers” and  “Jamjoom Pharmaceuticals Co” " ,
+            "Member of the board of “Al-Madina Newspaper and  Printing” ",
+            "Founder and member of the board of “The Sandwich  Gallery Co” and “Rufaidah Pharmacy Chain”",
+            "Founder of “Guru Advertising Promotional Items"
+        ],
+        Qualifications: [
+            "BSC and Mpharm – Ibn Sina Medical College",
+            "MBA – MCE, Belgium",
+            "Management Acceleration Program – INSEAD, Paris"
+        ]
+    },
+    {
+        name: "Olivier Hochstaettler",
+        about: "Olivier is currently Managing Partner at Five Capital and  member of the Executive Committee, a private equity  growth fund with main focus on the GCC region. He also  served as advisor, board and committee member of  several companies. Olivier has more than 20 years  investment experience inc. 17 years in the MENA region",
+        Work_Experience: [
+            "Former member of the board Careem Inc.",
+            "Senior Advisor to the CEO, Kingdom Holding Company (investment  holding)",
+            "Vice-Chairman Suez Saudi Environmental Services (industrial)",
+            "Member of the board of Webedia Arabia (digital entertainment)",
+            "Former Director Domestic Investments at Kingdom Holding Company,Director Investment Banking at Swicorp (KSA and UAE), Senior  Consultant at Price water house Coopers in Switzerland and Teaching  Assistant at HEC, Business School, University of Lausanne in Switzerland"
+        ],
+        Qualifications: [
+            "Master of Science, major in Finance, HEC, Business  School, University of Lausanne in Switzerland",
+            "Swiss Bachelor in Economic Sciences, College  Champittet,"
+        ]
+    },
+
+]
