7952fc67c21c3174ca553bb5f210a8c20bf7a385
AJ <joysondsouzaaj@gmail.com>
2023-06-27 20:07:22 +0530

    Client Data and image array set-Up

---
+++ src/assets/Data/Clients/clientImage.js
@@ -0,0 +1,32 @@
+import ikea from "../../Images/Clients/01_IKEA.jpg"
+import Flow from "../../Images/Clients/02_Flow.jpg"
+import Noon from "../../Images/Clients/03_Noon.jpg"
+import Aramex from "../../Images/Clients/04_Aramex.jpg"
+import Meridien from "../../Images/Clients/05_Meridien.png"
+import Almajdouie from "../../Images/Clients/06_Almajdouie.png"
+import Panda from "../../Images/Clients/07_Panda.jpg"
+import DAR from "../../Images/Clients/08_DAR.png"
+import SPL from "../../Images/Clients/09_SPL.png"
+import DACO from "../../Images/Clients/10_DACO.jpg"
+import SARED from "../../Images/Clients/11_SARED.jpg"
+import AMAZON from "../../Images/Clients/12_AMAZON.png"
+import Laderach from "../../Images/Clients/13_Laderach.jpg"
+import Smsa_Express from "../../Images/Clients/14_Smsa_Express.png"
+import DHL from "../../Images/Clients/15_DHL.png"
+import Honeywell from "../../Images/Clients/16_Honeywell.png"
+import Riyadh_Airport from "../../Images/Clients/17_Riyadh_Airport.png"
+import King_Abdullah_city from "../../Images/Clients/18_King_Abdullah_city.jpg"
+import King_Abdullah_university from "../../Images/Clients/19_King_Abdullah_university.jpg"
+import Alshrouq_express from "../../Images/Clients/20_Alshrouq_express.jpg"
+import Arabian_Food_Suppliers from "../../Images/Clients/21_Arabian_Food_Suppliers.jpg"
+import Al_Baik from "../../Images/Clients/22_Al_Baik.jpg"
+import patchi from "../../Images/Clients/23_patchi.jpg"
+import Algharbia_Farms from "../../Images/Clients/24_Algharbia_Farms.jpg"
+import Refsol from "../../Images/Clients/25_Refsol.jpg"
+
+export const clientImage =[
+    ikea, Flow, Noon, Aramex, Meridien, Almajdouie, Panda, DAR, SPL, DACO,
+    SARED, AMAZON, Laderach, Smsa_Express, DHL, Honeywell, Riyadh_Airport,
+    King_Abdullah_city, King_Abdullah_university, Alshrouq_express,
+    Arabian_Food_Suppliers, Al_Baik, patchi, Algharbia_Farms, Refsol,
+]
