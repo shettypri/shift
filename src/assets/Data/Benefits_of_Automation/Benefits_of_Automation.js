703cd1c40bace6f8fea029e9fcac8b4fce49aa7e
AJ <joysondsouzaaj@gmail.com>
2023-06-27 21:04:47 +0530

    Benefits_of_Automation.js is completed

---
+++ src/assets/Data/Benefits_of_Automation/Benefits_of_Automation.js
@@ -0,0 +1,27 @@
+import High_Efficiency from "../../Images/Benefits_of_Automation/01_High_Efficiency.png"
+import High_Control_mechanism from "../../Images/Benefits_of_Automation/02_High_Control_mechanism.png"
+import Lean_operations from "../../Images/Benefits_of_Automation/03_Lean_operations.png.png"
+import Compititive_Pricing from "../../Images/Benefits_of_Automation/04_Compititive_Pricing.png.png"
+
+export  const Benefits_of_Automation =[
+    {
+        Title:"High Efficiency",
+        Image:High_Efficiency,
+        Description:"Solutions that give you  the speed, agility and  quality you need to  maintain a competitive  edge"
+    },
+    {
+        Title:"High Control Mechanism",
+        Image:High_Control_mechanism,
+        Description:"Blending hardware and  software solutions to  obtain stronger oversight  over operations & ensure  high quality standards for  our customers"
+    },
+    {
+        Title:"Lean Operations\n",
+        Image:Lean_operations,
+        Description:"Greater customer  satisfaction while using as  few resources as possible;  creating value and  eliminating waste"
+    },
+    {
+        Title:"Competitive Pricing",
+        Image:Compititive_Pricing,
+        Description:"Reducing our cost  structures and passing  on the benefits to our  costumers"
+    },
+]
