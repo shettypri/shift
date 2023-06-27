b49262dd1b2ac8e0c8e2ca30db6bccd68e924423
AJ <joysondsouzaaj@gmail.com>
2023-06-27 20:32:41 +0530

    Founders

--- src/assets/Data/Founders/Founders.js
+++ src/assets/Data/Founders/Founders.js
@@ -1,4 +1,4 @@
-export const foundres = [{
+export const Founders = [{
     "Founder1": {
         name: "Khalid G. Al-Sulaiman",
         about: "Khalid is the Founder & Chief Executive Officer  of SHIFT & Managing Director of Generation C.  He is also a board member at SHIFT, Alsulaiman  Investments, & iMENA Group. He oversees the  operations and ensures the delivery of high  value services. A business leader with a proven  track record, an expert in cost optimization and  business process reengineering.",
